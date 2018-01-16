ee7b697cfb5c0e47ac62b7d3a6a7f5573bc9515a
Carles Hernandez-Ferrer <kuragari.ch@gmail.com>
2018-01-16 16:29:15 +0100

    Updated method 'enrich'.

--- R/CTDQuery-enrich.R
+++ R/CTDQuery-enrich.R
@@ -1,55 +1,55 @@
 #' @describeIn CTDdata Method to perform enrichment analysis given two
 #' object of class \code{CTDdata}.
-#' @note The pairs of \code{CTDdata} objects given to x and y must follow a
-#' specific pattern: gene-disease, gene-chemical or disease-chemical.
+#' @param universe String vector of genes used as universe. If not provided,
+#' all genes in CTDbase are used.
+#' @param use Select if all or only curated relations are used.
+#' @param warnings Shows or hiddes warnings.
 #' @param ... NOT USED
 setMethod(
     f = "enrich",
     signature = "CTDdata",
-    definition = function( x, y, ... ) {
+    definition = function( x, y, universe, use = "curated",
+                           warnings = TRUE, ... ) {
+        use <- match.arg( tolower( use ), choices = c( "all", "curated" ) )
+
+        if( missing( universe ) ) {
+            universe <- get( "universe_gene" )
+            #data( "universe_gene", package = "CTDquerier" )
+            #universe = universe_gene; rm( universe_gene )
+        } else {
+            universe <- unique( universe )
+        }
+
         if( x@type == "GENE" ) {
-            if( y@type == "DISEASE" | y@type == "CHEMICAL" ) {
-                this_gene <- get_terms( x )[[ "found" ]]
-                inthis_gene <- psygenet2r::extract( y, index_name = "gene interactions" )
-                inthis_gene <- unique( inthis_gene$Gene.Symbol )
-
-                download_ctd_genes()
-                all_genes <- unique( load_ctd_gene()$GeneSymbol )
-
-                mtr <- matrix( c(
-                    length( intersect( this_gene, inthis_gene ) ),
-                    sum( !this_gene %in% inthis_gene ),
-                    length( inthis_gene ) - length( intersect( this_gene, inthis_gene ) ),
-                    length( all_genes ) - length( inthis_gene )
-                ), ncol = 2, byrow = TRUE )
+            x <- get_terms( x )[[ "found" ]]
+        } else {
+            tbl <- get_table( x, index_name = "gene interactions" )
+            if( use == "curated" ) {
+                x <- unique( tbl[tbl$Direct.Evidence != "", "Gene.Symbol" ] )
             } else {
-                stop( "Invalid argument 'y'. Only 'disease' or 'chemical' ",
-                      "can be paired with 'gene' query.")
-            }
-
-        } else if ( x@type == "DISEASE" ) {
-            if( y@type == "CHEMICAL" ) {
-                this_gene <- psygenet2r::extract( y, index_name = "gene interactions" )
-                inthis_gene <- psygenet2r::extract( y, index_name = "gene interactions" )
-                this_gene <- unique( this_gene$Gene.Symbol )
-                inthis_gene <- unique( inthis_gene$Gene.Symbol )
-
-                download_ctd_genes()
-                all_genes <- unique( load_ctd_gene()$GeneSymbol )
-
-                mtr <- matrix( c(
-                    length( intersect( this_gene, inthis_gene ) ),
-                    sum( !this_gene %in% inthis_gene ),
-                    length( inthis_gene ) - length( intersect( this_gene, inthis_gene ) ),
-                    length( all_genes ) - length( inthis_gene )
-                ), ncol = 2, byrow = TRUE )
-
-            } else {
-                stop( "Invalid argument 'y'. Only 'chemical' can be paired ",
-                    "with 'disease' query.")
+                x <- unique( tbl$Gene.Symbol )
             }
         }
 
-        fisher.test( mtr, alternative = "greater" )
+        if( y@type == "GENE" ) {
+            y <- get_terms( y )[[ "found" ]]
+        } else {
+            tbl <- get_table( y, index_name = "gene interactions" )
+            if( use == "curated" ) {
+                if( !"Direct.Evidence" %in% colnames( tbl ) ) {
+                    if( warnings ) {
+                        warning( "Using 'CTDdata' from chemical query. ",
+                            "No curated assocations can be extracted." )
+                    }
+                    y <- unique( tbl$Gene.Symbol )
+                } else {
+                    y <- unique(tbl[tbl$Direct.Evidence != "", "Gene.Symbol"])
+                }
+            } else {
+                y <- unique( tbl$Gene.Symbol )
+            }
+        }
+
+        return( fisher.test( table( universe %in% y, universe %in% x ) ) )
     }
 )
