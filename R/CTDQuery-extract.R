fa0786429e17da745c6c2ca79b10cec45ee2961c
Carles Hernandez-Ferrer <kuragari.ch@gmail.com>
2018-01-16 16:34:29 +0100

    Changed method 'extract' to 'get_table'.

--- R/CTDQuery-extract.R
+++ R/CTDQuery-extract.R
@@ -1,39 +1,29 @@
 #' @describeIn CTDdata Method to obtain a specific inner table from an \code{CTDdata} object.
 #' @param object Object of class \code{CTDdata}.
-#' @details Available tables are (\code{index_name}):
-#' \enumerate{
-#'   \item \code{"genes interactions"}: (Only for chemicals) Table with a relation
-#'   of the genes interacting with the given chemicals.
-#'   \item \code{"chemicals interactions"}: (Only for genes) Table with a relation
-#'   of the chemicals interacting with the given genes.
-#'   \item \code{"diseases"}: Table with a relation of the diseases associated with
-#'   given genes or chemicals.
-#'   \item \code{"gene-gene interactions"}: (Only for genes) Table with a relation
-#'   of the genes interacting with the given genes.
-#'   \item \code{"kegg pathways"}: Table with a relation of the KEGG pathways affected by the
-#' given chemicals or where the given genes play a role.
-#'   \item \code{"go terms"}: Table with a relatio of the GO terms afected by the given
-#' chemicals or where the given genes play a role.
-#' }
-
-setMethod( "extract",
-  signature  = "CTDdata",
-  definition = function( object, index_name, ... ) {
-      index_name <- base::tolower( index_name )
-    if( index_name == "gene interactions" ) {
-      return( object@gene_interactions )
-    } else if( index_name == "chemical interactions" ) {
-      return( object@chemicals_interactions )
-    } else if( index_name == "diseases" ) {
-      return( object@diseases )
-    } else if( index_name == "gene-gene interactions" ) {
-      return( object@gene_gene_interactions )
-    } else if( index_name == "kegg pathways" ) {
-      return( object@kegg )
-    } else if( index_name == "go terms" ) {
-      return( object@go )
-    } else {
-      stop( "Invalid provided 'index_name'. ")
+setMethod( "get_table",
+    signature  = "CTDdata",
+    definition = function( object, index_name, ... ) {
+        index_name <- base::tolower( index_name )
+        switch( index_name,
+            "gene interactions" = {
+                return( object@gene_interactions )
+             },
+            "chemical interactions" = {
+                return( object@chemicals_interactions )
+            },
+            "diseases" = {
+                return( object@diseases )
+            },
+            "gene-gene interactions" = {
+                return( object@gene_gene_interactions )
+            },
+            "kegg pathways" = {
+                return( object@kegg )
+            },
+            "go terms" = {
+                return( object@go )
+            },
+            stop( "Invalid provided 'index_name'. ")
+        )
     }
-  }
 )
