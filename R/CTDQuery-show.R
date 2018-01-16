23a66b45e386720c6c7c83c32375ec0ef2019b09
Carles Hernandez-Ferrer <kuragari.ch@gmail.com>
2018-01-16 16:27:06 +0100

    Updated method 'show'.

--- R/CTDQuery-show.R
+++ R/CTDQuery-show.R
@@ -2,26 +2,75 @@ setMethod( "show",
     signature  = "CTDdata",
     definition = function( object ) {
         cat( "Object of class 'CTDdata'\n" )
-        cat( "--------------------------------\n" )
+        cat( "-------------------------\n" )
         cat( " . Type:", object@type, "\n" )
         cat( " . Length:", nrow( object@terms ), "\n")
         if( nrow( object@terms ) == 1 ) {
             it <- object@terms[ 1, 1 ]
         } else {
-            it <- paste0( object@terms[ 1,  1 ], ", ..., " , object@terms[ nrow( object@terms ), 1 ] )
+            it <- paste0( object@terms[ 1,  1 ], ", ..., " ,
+                          object@terms[ nrow( object@terms ), 1 ] )
         }
         cat( " . Items:", it, "\n" )
-        cat( " . #Diseases:", nrow( object@diseases ), "\n" )
         if( object@type == "GENE" ) {
-            cat( " . #Gene-gene interactions:", nrow( object@gene_gene_interactions ), "\n" )
-            cat( " . #Gene-chemical interactions:", nrow( object@chemicals_interactions ), "\n" )
+            xx <- as.data.frame(
+                table( !is.na( object@diseases[ , "Direct.Evidence" ] ) ) )
+            cat( " . Diseases:", length( unique( object@diseases$Disease.ID )),
+                 "(", xx[2,2], "/", nrow( object@diseases ), ")\n" )
+            cat( " . Gene-gene interactions:",
+                 nrow( unique(
+                     object@gene_gene_interactions[ ,
+                        c("Source.Gene.ID", "Target.Gene.ID" ) ] ) ),
+                 "(", nrow( object@gene_gene_interactions ), ")\n" )
+            cat( " . Gene-chemical interactions:",
+                 nrow( unique(
+                     object@chemicals_interactions[ ,
+                        c("Chemical.ID", "GeneID" ) ] ) ),
+                 "(", nrow( object@chemicals_interactions ), ")\n" )
+            xx <- "GeneID"
         } else if( object@type == "CHEMICAL" ) {
-            cat( " . #Chemical-gene interactions:", nrow( object@gene_interactions ), "\n" )
+            xx <- as.data.frame(
+                table( object@diseases[ , "Direct.Evidence" ] != "" ) )
+            cat( " . Diseases:", length( unique( object@diseases$Disease.ID )),
+                 "(", xx[2,2], "/", nrow( object@diseases ), ")\n" )
+            cat( " . Diseases:", length( unique( object@diseases$Disease.ID )),
+                 "(", nrow( object@diseases ), ")\n" )
+            cat( " . Chemical-gene interactions:",
+                 nrow( unique(
+                     object@gene_interactions[ ,
+                        c("Chemical.ID", "Gene.ID" ) ] ) ),
+                 "(", nrow( object@gene_interactions ), ")\n" )
+            xx <- "ChemicalID"
         } else if( object@type == "DISEASE" ) {
-            cat( " . #Disease-gene interactions:", nrow( object@gene_interactions ), "\n" )
-            cat( " . #Disease-chemical interactions:", nrow( object@chemicals_interactions ), "\n" )
+            xx <- as.data.frame(
+                table( object@gene_interactions[ , "Direct.Evidence" ] != "" ))
+            cat( " . Disease-gene interactions:",
+                 nrow( unique(
+                     object@gene_interactions[ ,
+                        c("Disease.ID", "Gene.ID" ) ] ) ),
+                 "(", xx[2,2], "/", nrow( object@gene_interactions ), ")\n" )
+            xx <- as.data.frame( table(
+                object@chemicals_interactions[ , "Direct.Evidence" ] != "" ) )
+            cat( " . Gene-chemical interactions:",
+                 nrow( unique(
+                     object@chemicals_interactions[ ,
+                        c("Chemical.ID", "Disease.ID" ) ] ) ),
+                 "(", xx[2,2], "/", nrow(object@chemicals_interactions),")\n")
+            xx <- "Disease.ID"
         }
-        cat( " . #KEGG pathways:", nrow( object@kegg ), "\n" )
-        cat( " . #GO terms:", nrow( object@go ), "\n" )
+        if( nrow( object@kegg ) > 0 ) {
+            cat( " . KEGG pathways:",
+                 nrow( unique( object@kegg[ , c( "Pathway.ID", xx ) ] ) ),
+                 "(", nrow( object@kegg ), ")\n" )
+        } else {
+            cat( " . KEGG pathways: 0 (-)\n" )
+        }
+        if( nrow( object@go ) > 0 ) {
+            cat( " . GO terms:",
+                 nrow( unique( object@go[, c( "GO.Term.ID", xx ) ] ) ),
+                 "(", nrow( object@go ), ")\n" )
+        } else {
+            cat( " . GO terms: 0 (-)\n" )
+        }
     }
 )
